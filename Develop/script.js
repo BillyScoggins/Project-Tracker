8e9d0496a03935c0560f214d4f2abfe1142b1f5b
Billy Scoggins <118499026+BillyScoggins@users.noreply.github.com>
2023-03-26 18:33:34 -0500

    working with LA

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,6 @@ $(function () {
 
 
 
-
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -15,7 +14,7 @@ $(function () {
   // useful when saving the description in local storage?
 
   $(".saveBtn").click(function () {
-    
+
     console.log($(this).parent())
   });
   //
@@ -27,25 +26,25 @@ $(function () {
 
 
 
-  function timeTracker () {
+  function timeTracker() {
     var currentTime = dayjs().format("dddd");
     console.log(currentTime);
-  
+
     var currentMonth = dayjs().format("MMMM");
     var currentDate = dayjs().format("DD");
     var currentYear = dayjs().format("YYYY");
-    
+
 
     //  jquery loops specifically .each() method
     //in this loop yoou lop over all the .timeblock classes so computer has acess to all time blocks 
     //identify each time block in a new variable representing specific the hour in the time block 
     // specific block hour is same as curretn time then we are in future class, if block hour is before current time we are in future etc... 
-// create an if statment that changes the classes based on the line above with addclass() and remove class() jquery methods
+    // create an if statment that changes the classes based on the line above with addclass() and remove class() jquery methods
     $("#currentDay").text(currentTime);
     $("#currentMonth").text(currentMonth);
     $("#currentDate").text(currentDate);
     $("#currentYear").text(currentYear);
-    
+
 
   }
 
@@ -57,4 +56,29 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page    
 
-})
+function highliter() {
+  var hour = dayjs().hour();
+  console.log(hour)
+
+
+
+  $(".time-block").each(function () {
+    var current = parseInt($(this).attr('id').split('-')[1]);
+
+    if (current > hour) {
+      $(this).addClass("future");
+
+    } else if (current < hour) {
+      $(this).addClass("past");
+    }
+
+    else {
+      $(this).addClass("present");
+
+    }
+  });
+}
+highliter()
+
+
+});
